12daf9de89f09b0951e74d9237b9b859809d2c8e
Tanishq Dubey <tanishq.dubey@gmail.com>
2018-04-08 14:21:07 -0500

    Cleaner module

--- full_adder.v
+++ full_adder.v
@@ -1,27 +1,18 @@
-module full_adder 
-  (
-   i_bit1,
-   i_bit2,
-   i_carry,
-   o_sum,
-   o_carry
-   );
- 
-  input  i_bit1;
-  input  i_bit2;
-  input  i_carry;
-  output o_sum;
-  output o_carry;
- 
-  wire   w_WIRE_1;
-  wire   w_WIRE_2;
-  wire   w_WIRE_3;
-       
-  assign w_WIRE_1 = i_bit1 ^ i_bit2;
-  assign w_WIRE_2 = w_WIRE_1 & i_carry;
-  assign w_WIRE_3 = i_bit1 & i_bit2;
- 
-  assign o_sum   = w_WIRE_1 ^ i_carry;
-  assign o_carry = w_WIRE_2 | w_WIRE_3;
+module full_adder (
+    input a,
+    input b,
+    input cin,
+    output s,
+    output cout
+);
+
+    wire w1, w2, w3;
+
+    and( w1, a, b );
+    and( w2, a, cin );
+    and( w3, b, cin );
+    or( cout, w1, w2, w3 );
+
+    xor( s, a, b, cin );
 
 endmodule
